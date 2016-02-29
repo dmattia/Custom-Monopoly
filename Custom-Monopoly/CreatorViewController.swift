43ade68a0985fd42ce1c79c4f253a2c723cce79c
dmattia <dmattia@nd.edu>
2016-02-28 19:46:43 -0500

    Centered cells of collectionView

--- Custom-Monopoly/CreatorViewController.swift
+++ Custom-Monopoly/CreatorViewController.swift
@@ -7,10 +7,12 @@
 //
 
 import UIKit
+import KTCenterFlowLayout
 
 class CreatorViewController : UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
     
     @IBOutlet weak var boardSpaceCollectionView: UICollectionView!
+    private let counts : [Int] = [2, 3, 2, 3, 2, 2, 3, 3, 4, 4, 4]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +21,6 @@ class CreatorViewController : UIViewController, UICollectionViewDataSource, UICo
         self.boardSpaceCollectionView.dataSource = self
         
         let collectionViewLayout : UICollectionViewFlowLayout = self.boardSpaceCollectionView.collectionViewLayout as! UICollectionViewFlowLayout
-        collectionViewLayout.sectionInset = UIEdgeInsets(top: 20, left: 0, bottom: 20, right: 0)
         collectionViewLayout.headerReferenceSize = CGSize(width: 0, height: 30)
     }
     
@@ -40,6 +41,34 @@ class CreatorViewController : UIViewController, UICollectionViewDataSource, UICo
             }
     }
     
+    func collectionView(collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        insetForSectionAtIndex section: Int) -> UIEdgeInsets {
+            
+            let itemsInSection = self.boardSpaceCollectionView.numberOfItemsInSection(section)
+            let frameWidth = self.boardSpaceCollectionView.frame.width
+            //let screenWidth = UIScreen.mainScreen().bounds.width
+            let imageWidth = (frameWidth / 5)
+            let left_margin = (frameWidth - imageWidth * CGFloat(itemsInSection)) / 2.0
+            
+            print("The frame is \(frameWidth) wide")
+            print("The width of the images are \(imageWidth)")
+            print("There are \(itemsInSection) items in section \(section)")
+            print("Left margins: \(left_margin)")
+            print("-------------------------------------")
+            
+            return UIEdgeInsets(top: 20,
+                left: left_margin - 20,
+                bottom: 20,
+                right: left_margin - 20)
+    }
+    
+    func collectionView(collectionView: UICollectionView,
+        layout collectionViewLayout: UICollectionViewLayout,
+        minimumLineSpacingForSectionAtIndex section: Int) -> CGFloat {
+            return 50.0
+    }
+
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let cell = self.boardSpaceCollectionView.dequeueReusableCellWithReuseIdentifier("collectionCellId", forIndexPath: indexPath)
         
@@ -47,7 +76,7 @@ class CreatorViewController : UIViewController, UICollectionViewDataSource, UICo
     }
     
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return random() % 3 + 2
+        return counts[section]
     }
     
     func collectionView(collectionView: UICollectionView, canMoveItemAtIndexPath indexPath: NSIndexPath) -> Bool {
@@ -57,12 +86,13 @@ class CreatorViewController : UIViewController, UICollectionViewDataSource, UICo
     func collectionView(collectionView: UICollectionView,
         layout collectionViewLayout: UICollectionViewLayout,
         sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
-            let screenWidth = UIScreen.mainScreen().bounds.width
-            let imageWidth = screenWidth / 5
+            let frameWidth = self.boardSpaceCollectionView.frame.width
+            //let screenWidth = UIScreen.mainScreen().bounds.width
+            let imageWidth = frameWidth / 5
             return CGSize(width: imageWidth, height: imageWidth)
     }
     
     func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
-        return 20
+        return counts.count
     }
 }
